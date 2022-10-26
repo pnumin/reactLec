4ed5d43be286e082ee06381344e44188ff44ad85
pnumin <49543573+pnumin@users.noreply.github.com>
2022-10-26 17:51:00 +0900

    4주차 수업자료

---
+++ 04/App.js
@@ -0,0 +1,16 @@
+import { Route, Routes} from 'react-router-dom' ;
+import MvList from './mv2/MvList' ;
+import MvInfoMain from './mv2/MvInfoMain' ;
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element = {<MvList />} /> 
+        <Route path='/MvInfoMain/:mvcd' element = {<MvInfoMain />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
